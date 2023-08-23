4f5b3fffcd723ed6ae5f1c7c49beb95a667ddc36
001Smiling <alena7846@mail.ru>
2023-08-23 17:01:50 +0300

    Trinity scss-mob-table

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-const video = document.getElementById('my-video');
-const playBtn = document.getElementById('play-btn');
-const pauseBtn = document.getElementById('pause-btn');
-const overlay = document.getElementById('overlay');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamb = document.querySelector("#hamb");
+  const popup = document.querySelector("#popup");
+  const menu = document.querySelector("#menu").cloneNode(1);
+  const body = document.body;
+  const links = Array.from(menu.children);
 
-playBtn.addEventListener('click', () => {
-  video.play();
-  overlay.classList.add('fade-out');
-});
+  hamb.addEventListener("click", hambHandler);
 
-video.addEventListener('ended', () => {
-  overlay.classList.remove('fade-out');
-});
+  function hambHandler(e) {
+    e.preventDefault();
+    popup.classList.toggle("open");
+    hamb.classList.toggle("active");
+    body.classList.toggle("noscroll");
+    renderPopap();
+  }
+
+  function renderPopap() {
+    popup.appendChild(menu);
+  }
+
+  links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+  });
+
+  function closeOnClick() {
+    popup.classList.remove("open");
+    hamb.classList.remove("active");
+    body.classList.remove("noscroll");
+  }
+})
+
+
+// const video = document.getElementById('my-video');
+// const playBtn = document.getElementById('play-btn');
+// const pauseBtn = document.getElementById('pause-btn');
+// const overlay = document.getElementById('overlay');
+
+// playBtn.addEventListener('click', () => {
+//   video.play();
+//   overlay.classList.add('fade-out');
+// });
+
+// video.addEventListener('ended', () => {
+//   overlay.classList.remove('fade-out');
+// });
 
 
 
